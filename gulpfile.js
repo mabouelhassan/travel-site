72bf95fbb8bf506cd7536d50ef98dcf052fdda0a
mabouelhassan <mabouelhassan@gmail.com>
2018-05-28 09:31:23 -0600

    End of Gulp lesson

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default', function(){
+    console.log('Horray -- gulp');
+});
+
+gulp.task('html', function(){
+    console.log('Html task');
+});
